d5ad6d47b60062bd07db21935205b5a53e390ded
Vitali Kozhar <voxfree@pm.me>
2025-01-02 22:42:35 +0400

    solution: solution task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,36 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  console.log(members);
+  if (!Array.isArray(members)) return false;
+  let nameResult = [];
+  for (let i = 0; i < members.length; i += 1) {
+    if (typeof members[i] === "string") {
+      members[i] = members[i].trim();
+    }
+    if (typeof members[i] === "string") {
+      nameResult.push(members[i][0].toUpperCase());
+    }
+  }
+  if (nameResult.length === 0) return false;
+  const result = nameResult.sort();
+  console.log(result.join(""));
+  return result.join(""); // remove line with error and write your code here
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
