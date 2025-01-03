5f19a27adca74e290e5258cdc2521df98470b434
Vitali Kozhar <voxfree@pm.me>
2025-01-03 15:54:33 +0400

    solution: solution task

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const result = [];
+  const tempArr = [];
+  const resultArray = [];
+  let indexFirst;
+  let tempNameFirst;
+  let indexSecond;
+  let tempNameSecond;
+  for (let i = 0; i < names.length; i += 1) {
+    tempNameFirst = "";
+    tempNameSecond = "";
+    indexFirst = 0;
+    indexSecond = 0;
+    if (result.includes(names[i])) {
+      result.forEach((element) => {
+        if (names[i] === element) {
+          indexFirst += 1;
+        }
+      });
+      tempNameFirst = `${names[i]}(${indexFirst})`;
+    } else {
+      tempNameFirst = names[i];
+    }
+    result.push(names[i]);
+
+    if (tempArr.includes(tempNameFirst)) {
+      tempArr.forEach((element) => {
+        if (tempNameFirst === element) {
+          indexSecond += 1;
+        }
+      });
+      tempNameSecond = `${tempNameFirst}(${indexSecond})`;
+    } else {
+      tempNameSecond = tempNameFirst;
+    }
+    tempArr.push(tempNameFirst);
+    resultArray.push(tempNameSecond);
+  }
+
+  return resultArray; // remove line with error and write your code here
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
